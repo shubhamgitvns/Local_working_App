386522d7bbe3090170bd530b5a63db0ed68094dc
shubhamgitvns <shubhammaurya10092002@gmail.com>
2023-12-27 19:14:48 +0530

    Create app bar in the client form page

--- lib/Client_form_page.dart
+++ lib/Client_form_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:githubtesting/App_Theam.dart';
 
 class Client_form_Page extends StatefulWidget {
   const Client_form_Page({super.key});
@@ -10,6 +11,24 @@ class Client_form_Page extends StatefulWidget {
 class _Client_form_PageState extends State<Client_form_Page> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: App_Theam.White,
+      ),
+      body: Container(
+        color: App_Theam.White,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Detail page")
+              ],
+            )
+          ],
+        ),
+      ),
+
+    );
   }
 }
