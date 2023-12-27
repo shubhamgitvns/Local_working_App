b9beb02826fe30a0847fac50fb3eca21153cf8e6
shubhamgitvns <shubhammaurya10092002@gmail.com>
2023-12-27 23:35:14 +0530

    Add the navigator.push on the edit button

--- lib/Client_Home_Page.dart
+++ lib/Client_Home_Page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:githubtesting/App_Theam.dart';
+import 'package:githubtesting/Client_form_page.dart';
 class Client_Home_Page extends StatefulWidget {
   const Client_Home_Page({super.key});
 
@@ -62,30 +63,40 @@ class _Client_Home_PageState extends State<Client_Home_Page> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                        Container(
-                         height: 30,
-                          width: 100,
-                          
-                          decoration: BoxDecoration(
-                            color: App_Theam.Orange,
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(color: App_Theam.border_color),
-                            boxShadow: [
-                          const BoxShadow(
-                          color: Colors.transparent,
-                            offset: Offset(2.0, 2.0),
-                            blurRadius: 10,
-                            spreadRadius: 1.0,
+                        InkWell(
+                          child: Container(
+                           height: 30,
+                            width: 100,
+
+                            decoration: BoxDecoration(
+                              color: App_Theam.Orange,
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(color: App_Theam.border_color),
+                              boxShadow: [
+                            const BoxShadow(
+                            color: Colors.transparent,
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 10,
+                              spreadRadius: 1.0,
+                            ),
+                            BoxShadow(
+                              color: App_Theam.bgcolor,
+                              offset: Offset(-2.0, -2.0),
+                              blurRadius: 10,
+                              spreadRadius: 1.0,
+                            ),
+                            ]
+                            ),
+                            child: const Center(child: Text("Edit Profile")),
                           ),
-                          BoxShadow(
-                            color: App_Theam.bgcolor,
-                            offset: Offset(-2.0, -2.0),
-                            blurRadius: 10,
-                            spreadRadius: 1.0,
-                          ),
-                          ]
-                          ),
-                          child: const Center(child: Text("Edit Profile")),
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                    const Client_form_Page()));
+
+                          },
                         )
                       ],)
                     ],
